a8ef4a6a687c08501db842bbc473b42f0af2f64f
hgdhot <hgdhot@163.com>
2019-03-07 11:11:07 +0800

    定义GoodsSerializer，添加相应注释

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
 
+from goods.models import Goods, GoodsCategory
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+# class GoodsSerializer(serializers.Serializer):
+class GoodsSerializer(serializers.ModelSerializer):
+
+    # name = serializers.CharField(required=True, max_length=100)
+    # click_num = serializers.IntegerField(default=0)
+    # goods_front_image = serializers.ImageField()
+
+    def create(self, validated_data):
+        """
+        用校验后的数据validated_data创建一个新的对应模型类对象
+        """
+        return Goods.objects.create(**validated_data)
+
+    # 定义序列化器时，这样一个字段一个字段的定义很麻烦，可以使用模型序列化器
+    class Meta:
+        model = Goods
+        # 通过fields属性指明模型类的哪些字段需要序列化
+        # 通过__all__制定字段，默认外键字段的值是外键值
+        # 可以手动覆盖，使其显示外键对象的完整属性
+        fields = '__all__'
+    # 自定义外键字段的序列化器，使其显示外键关联对象的完整属性
+    category = CategorySerializer()
+
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,5 +1,5 @@
 from .serializers import GoodsSerializer
-from rest_framework.views import APIView
+from rest_framework.views import APIView, status
 from rest_framework.response import Response
 
 from .models import Goods
@@ -11,7 +11,19 @@ class GoodsListView(APIView):
     """
     List all goods
     """
-    def get(self, request):
+    def get(self, request, format=None):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)
         return Response(goods_serializer.data)
+
+    def post(self, request, format=None):
+        # 此处的request对象时drf框架封装的
+        # 会把django的request对象中的GET/POST等请求参数都放到data属性中
+        serializer = GoodsSerializer(data=request.data)
+        # 序列化器可以用来做校验
+        if serializer.is_valid():
+            # 序列化器可以用来将请求数据保存到数据库中（类似于django的forms）
+            serializer.save()
+            # 在rest_framework.views中的status模块中定义了很多标准响应码
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
